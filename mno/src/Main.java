11214bc8fdd955e98821a43a0e03dbda1c1c8e20
fanevoda <59056171+fanevoda@users.noreply.github.com>
2023-12-07 20:28:38 +0200

    hallow pattern dragut

--- mno/src/Main.java
+++ mno/src/Main.java
@@ -1,17 +1,37 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    public static void printHallowPattern(int noOfRows, int noOfCols)
+    {
+        for (int i = 0; i < noOfCols; i++)
+        {
+            System.out.print('*');
         }
+
+        System.out.print('\n');
+
+        for (int i = 0; i < noOfRows - 2; i++)
+        {
+            System.out.print('*');
+
+            for (int j = 0; j < noOfCols - 2; j++)
+            {
+                System.out.print(' ');
+            }
+
+            System.out.print("*\n");
+
+        }
+
+        for (int i = 0; i < noOfCols; i++)
+        {
+            System.out.print('*');
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        printHallowPattern(5, 20);
     }
 }
